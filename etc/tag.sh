6d4a05d0d4ef742f6f7eb1db81392d0f11d083b4
Alexey Romanoff <ar@logiceditor.com>
2021-05-30 13:18:16 +0300

    etc/tag.sh: fix tag name

--- etc/tag.sh
+++ etc/tag.sh
@@ -2,7 +2,7 @@
 
 set -euox pipefail
 
-BASE_TAG="scm-0"
-TAG="$(git describe --tags --exclude "${BASE_TAG}-*-*" | sed 's/-/./g')"
+BASE_TAG="scm.0"
+TAG="$(git describe --tags --exclude "${BASE_TAG}.*.*" | sed 's/-/./g')"
 git tag "${TAG}"
 git push origin "${TAG}"
